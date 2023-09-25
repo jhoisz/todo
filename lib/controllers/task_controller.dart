6eed0874270266f1df0a125a9cca925105188845
jhoisz <jvrda@ufpi.edu.br>
2023-09-25 19:30:37 -0300

    Intermediário entre Data e interface do usuário

---
+++ lib/controllers/task_controller.dart
@@ -0,0 +1,27 @@
+import '../data/task_database.dart';
+import '../domain/usecases/task_usecases.dart';
+import '../domain/entities/task.dart';
+
+class TaskController implements TaskUsecases {
+  final TaskDatabase _taskDatabase = TaskDatabase();
+
+  @override
+  Future<void> addTask(Task task) async {
+    await _taskDatabase.save(task);
+  }
+
+  @override
+  Future<List<Task>> getTasks(Task task) async {
+    return await _taskDatabase.findAll();
+  }
+
+  @override
+  Future<void> removeTask(Task task) async {
+    await _taskDatabase.delete(task);
+  }
+
+  @override
+  Future<void> updateTask(Task task) async {
+    await _taskDatabase.save(task);
+  }
+}
