2f8222d0ba4ee2dc9feb9f7869baafe46902cbbe
jhoisz <jvrda@ufpi.edu.br>
2023-09-25 18:34:54 -0300

    Casos de uso das tarefas

---
+++ lib/domain/usecases/task_usecases.dart
@@ -0,0 +1,8 @@
+import '../entities/task.dart';
+
+abstract class TaskUsecases {
+  Future<void> addTask(Task task);
+  Future<void> updateTask(Task task);
+  Future<void> removeTask(Task task);
+  Future<List<Task>> getTasks(Task task);
+}
